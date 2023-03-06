504b495778bb05e0ef561b17e61132621da1f7a1
Myles Grant <myles@mylesgrant.com>
2023-03-06 15:11:46 -0800

    The basic handling of messages on stdin on a loop

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,10 @@
-fn main() {
-    println!("Hello, world!");
+use std::io;
+
+#[tokio::main]
+async fn  main() -> io::Result<()> {
+    loop {
+        let mut buffer = String::new();
+        io::stdin().read_line(&mut buffer)?;
+        eprint!("Received: {}", buffer);
+    }
 }
