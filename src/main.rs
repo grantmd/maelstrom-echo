60e98e14b039ddb0a6dcefe5fa26bb07fe8b6039
Myles Grant <myles@mylesgrant.com>
2023-03-06 16:29:19 -0800

    JSON handling and replying

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,22 @@
 use std::io;
+use serde::{Deserialize, Serialize};
+
+#[derive(Serialize, Deserialize)]
+struct MessageBody {
+    #[serde(rename(serialize = "type", deserialize = "type"))]
+    msg_type: String,
+    msg_id: u128,
+    #[serde(default)]
+    in_reply_to: u128,
+    echo: String,
+}
+
+#[derive(Serialize, Deserialize)]
+struct Message {
+    src: String,
+    dest: String,
+    body: MessageBody,
+}
 
 #[tokio::main]
 async fn  main() -> io::Result<()> {
@@ -6,5 +24,19 @@ async fn  main() -> io::Result<()> {
         let mut buffer = String::new();
         io::stdin().read_line(&mut buffer)?;
         eprint!("Received: {}", buffer);
+
+        let mut msg: Message = serde_json::from_str(&buffer)?;
+
+        let src = msg.src;
+        let dest = msg.dest;
+        msg.src = dest;
+        msg.dest = src;
+
+        msg.body.msg_type = "echo_ok".to_string();
+        msg.body.in_reply_to = msg.body.msg_id;
+
+        let out = serde_json::to_string(&msg)?;
+        eprintln!("Sending: {}", out);
+        println!("{}", out);
     }
 }
