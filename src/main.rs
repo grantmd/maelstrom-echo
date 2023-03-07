8e0ea620134c088ea96e290297ec654a977a84d3
Myles Grant <myles@mylesgrant.com>
2023-03-06 22:18:26 -0800

    Make a Node object for some state, begin to handle more message types

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,29 @@
 use std::io;
 use serde::{Deserialize, Serialize};
 
-#[derive(Serialize, Deserialize)]
+#[derive(Default, Debug)]
+struct Node {
+    id: String,
+    msg_id: u128,
+}
+
+#[derive(Default, Serialize, Deserialize, Debug)]
 struct MessageBody {
     #[serde(rename(serialize = "type", deserialize = "type"))]
     msg_type: String,
     msg_id: u128,
+
     #[serde(default)]
     in_reply_to: u128,
+
+    #[serde(default)]
+    node_id: String,
+
     #[serde(default)]
     echo: String,
 }
 
-#[derive(Serialize, Deserialize)]
+#[derive(Default, Serialize, Deserialize, Debug)]
 struct Message {
     src: String,
     dest: String,
@@ -21,27 +32,45 @@ struct Message {
 
 #[tokio::main]
 async fn  main() -> io::Result<()> {
+    let mut node: Node = Default::default();
+
     loop {
         let mut buffer = String::new();
         io::stdin().read_line(&mut buffer)?;
         eprint!("Received: {}", buffer);
 
-        let mut msg: Message = serde_json::from_str(&buffer)?;
-        if msg.body.msg_type != "echo" {
-            eprintln!("Skipping");
-            continue;
+        let msg: Message = serde_json::from_str(&buffer)?;
+        let body = msg.body;
+
+        let mut reply: MessageBody = Default::default();
+
+        match body.msg_type.as_str() {
+            "init" => {
+                node.id = body.node_id;
+                reply.msg_type = "init_ok".to_string();
+            },
+            "echo" => {
+                reply.msg_type = "echo_ok".to_string();
+                reply.echo = body.echo;
+            },
+            _ => {
+                eprintln!("Unknown message type: {}", body.msg_type);
+                continue;
+            }
         }
 
-        let src = msg.src;
-        let dest = msg.dest;
-        msg.src = dest;
-        msg.dest = src;
+        node.msg_id += 1;
+        reply.msg_id = node.msg_id;
 
-        msg.body.msg_type = "echo_ok".to_string();
-        msg.body.in_reply_to = msg.body.msg_id;
+        reply.in_reply_to = body.msg_id;
+        let out = Message {
+            src: node.id.clone(),
+            dest: msg.src,
+            body: reply,
+        };
 
-        let out = serde_json::to_string(&msg)?;
-        eprintln!("Sending: {}", out);
-        println!("{}", out);
+        let out_str = serde_json::to_string(&out)?;
+        eprintln!("Sending: {}", out_str);
+        println!("{}", out_str);
     }
 }
